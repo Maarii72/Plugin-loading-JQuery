6234d08c5d10ff745eb62f89bb7869b4693bb473
Marilena Sales <marilena.sales@gmail.com>
2021-01-16 21:19:07 -0300

    função para unload

--- hcode_loader.js
+++ hcode_loader.js
@@ -8,40 +8,58 @@
 (function($){
 
     //nome do plugin
-    $.fn.load =function(){
+    $.fn.load =function(action = 'load'){
 
         // garante com this.each que todos elementos serão afetados
         return this.each(function(){
 
-           let divToAppend = document.createElement('div')
-           divToAppend.id = 'loader'
-           $(divToAppend).css({
-                width: "100%",height:"100%",
-                position: "fixed",
-                top: 0,
-                left: 0,
-                "text-align": "center",
-                backgroundColor: "rgba(230, 5, 61, 0.795)"
-                 
+            switch(action){
+                case 'load':
                 
-           })
+                    let divToAppend = document.createElement('div')
+                    divToAppend.id = 'loader'
+                    $(divToAppend).css({
+                         width: "100%",height:"100%",
+                         position: "fixed",
+                         top: 0,
+                         left: 0,
+                         "text-align": "center",
+                         backgroundColor: "rgba(230, 5, 61, 0.795)"
+                          
+                         
+                    })
+         
+                     let divLoad = document.createElement('div')
+                     divLoad.className = 'lds-dual-ring'
+         
+                     $(divLoad).css({
+                         position: "absolute",
+                         top: "50%",
+                         left: "50%"
+                     })
+         
+                     //pegar a div load e adicionar dentro da div principal usando o método do jquery append
+         
+                     $(divToAppend).append(divLoad)
+         
+                     //adicionar a div ao body
+                     $(this).append(divToAppend)
+                
 
-            let divLoad = document.createElement('div')
-            divLoad.className = 'lds-dual-ring'
+                break;
 
-            $(divLoad).css({
-                position: "absolute",
-                top: "50%",
-                left: "50%"
-            })
+                case 'unload':
+                    $('#loader').fadeOut(function(){
+                        $(this).remove
+                    })     
 
-            //pegar a div load e adicionar dentro da div principal usando o método do jquery append
+                break
 
-            $(divToAppend).append(divLoad)
+                default:
+                    console.error('Informe uma ação válida')
+            }
 
-            //adicionar a div ao body
-            $(this).append(divToAppend)
-
+           
 
         })
     }
